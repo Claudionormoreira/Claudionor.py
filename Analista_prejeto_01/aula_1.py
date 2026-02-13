51dbac0f081dcf7e1ead9526e914a67039663528
Claudionor=MOREIRA <claudionormoreirarj@gmail.com>
2026-02-13 04:19:11 +0000

    commit

--- Analista_prejeto_01/aula_1.py
+++ Analista_prejeto_01/aula_1.py
@@ -1,5 +1,42 @@
-import tkinter
-janela = tkinter.Tk()
-janela.mainloop()
+import tkinter as tk
+
+def submit():
+    #Recuperar os dados dos campos de entrada
+    nome = nome_entry.get()
+    email = email_entry.get()
+
+    #Imprime os dados do console
+    print("Nome:", nome)
+    print("Email:", email)
+
+# Criar janela principal
+root = tk.Tk()
+root.title("Formulário de inscrição")
+
+# Criar um frame para conter os widgets
+frame = tk.Frame(root)
+frame.pack(padx=10, pady=10)
+
+# Campo de entrada para "Nome"
+nome_entry = tk.Entry(frame)
+nome_entry.grid(row=0, column=1)
+
+# Campo de entrada para "Email"
+email_entry = tk.Entry(frame)
+email_entry.grid(row=1, column=1)
+
+#Botão de submissão 
+submit_button = tk.Button(frame, tex="Submeter", command=submit)
+submit_button.grid(row=2, columnspan=2, pady=10)
+
+# Inicializar o loop da GUI
+root.mainloop()
+
+
+
+
+
+
+
 
 
